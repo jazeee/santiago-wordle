be6648e5ce6f667c708dee9eb91b87f624c1ca36
Jaz Singh <jazeee@gmail.com>
2024-05-06 15:44:59 -0700

    Convert to character count map to fix test

--- src/Wordle/utils.ts
+++ src/Wordle/utils.ts
@@ -2,6 +2,15 @@ import { GuessOutcome } from './types';
 
 export const ALLOWED_WORD_LENGTH = 5;
 
+function summarizeWordMetrics(word: string) {
+  const characterCount: Map<string, number> = new Map();
+  for (const character of word) {
+    const count = characterCount.get(character) ?? 0;
+    characterCount.set(character, count + 1);
+  }
+  return characterCount;
+}
+
 export function getMetrics(word: string, guessedWord: string): GuessOutcome[] {
   // if (
   //   word.length !== ALLOWED_WORD_LENGTH ||
@@ -9,17 +18,19 @@ export function getMetrics(word: string, guessedWord: string): GuessOutcome[] {
   // ) {
   //   throw new Error(`Invalid word lengths ${word}`);
   // }
-  const characters = new Set([...word]);
+  const characterCount = summarizeWordMetrics(word);
   // alpha validation.
   // upper case validation.
   const guessStates = [];
   for (let index = 0; index < ALLOWED_WORD_LENGTH; index += 1) {
     const character = word[index];
     const guessedCharacter = guessedWord[index];
+    const guessedCharacterCount = characterCount.get(guessedCharacter) ?? 0;
     if (character === guessedCharacter) {
       guessStates.push(GuessOutcome.MATCH_IN_POSITION);
-    } else if (characters.has(guessedCharacter)) {
+    } else if (guessedCharacterCount > 0) {
       guessStates.push(GuessOutcome.MATCH_IN_OTHER_POSITION);
+      characterCount.set(guessedCharacter, guessedCharacterCount - 1);
     } else {
       guessStates.push(GuessOutcome.NO_MATCH);
     }
